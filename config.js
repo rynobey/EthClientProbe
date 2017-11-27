0baa3a93d04835c8ec9e1eb8a0be36c49da55a69
Ryno Beyers <rynobey@gmail.com>
2017-11-27 15:34:32 +0200

    Added config file

---
+++ config.js
@@ -0,0 +1,14 @@
+var config = {}
+
+// http server listening port
+config.port = 8001;
+
+// local stats update time (itime over which stats will be averaged) in seconds
+config.period = 5;
+
+/* directory where blockchain data is stored (typically geth/chaindata). Needs
+   to be an absolute path, or a relative path from where the probe is run.
+*/   
+config.blockchainData = '../QuorumNetworkManager/Blockchain/geth/chaindata/';
+
+module.exports = config;
