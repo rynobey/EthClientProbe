10339751c12eb37daab800391cc1f9ca39de7e63
Ryno Beyers <rynobey@gmail.com>
2017-11-27 16:59:01 +0200

    Added arguments to call to diskStats.sh

--- diskStats.sh
+++ diskStats.sh
@@ -1,7 +1,15 @@
+#!/bin/bash
 iowait=`cat diskUtilization.tmp | head -4 | tail -1 | awk '{print $4}'`
 await=`cat diskUtilization.tmp | tail -2 | head -1 | awk '{print $10}'`
 svctm=`cat diskUtilization.tmp | tail -2 | head -1 | awk '{print $13}'`
 kBps_read=`cat diskUtilization.tmp | tail -2 | head -1 | awk '{print $6}'`
 kBps_wrtn=`cat diskUtilization.tmp | tail -2 | head -1 | awk '{print $7}'`
-timestamp=`date +%s%N | cut -b1-13`
-printf "$timestamp,$iowait,$await,$svctm,$kBps_read,$kBps_wrtn"
+if [ "$#" == 1 ]
+then
+  chaindata=`du -c $1 | tail -1 | awk '{print $1}'`
+  timestamp=`date +%s%N | cut -b1-13`
+  printf "$timestamp,$iowait,$await,$svctm,$kBps_read,$kBps_wrtn,$chaindata"
+else
+  timestamp=`date +%s%N | cut -b1-13`
+  printf "$timestamp,$iowait,$await,$svctm,$kBps_read,$kBps_wrtn"
+fi
