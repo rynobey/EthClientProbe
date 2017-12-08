79ff7de390f0f6ffc60c238f1afec71c731f2fd4
Ryno Beyers <rynobey@gmail.com>
2017-12-08 16:39:04 +0200

    Removed old scripts

--- cpuStats.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-loadavg_1m=`cat /proc/loadavg | awk '{print $1}'`
-loadavg_5m=`cat /proc/loadavg | awk '{print $2}'`
-loadavg_15m=`cat /proc/loadavg | awk '{print $3}'`
-cpu_utilization=`cat cpuUtilization.tmp | tail -2 | awk '{print $1}'`
-num_cpus=`lscpu | grep "CPU(s)" | head -1 | awk '{print $2}'`
-timestamp=`date +%s%N | cut -b1-13`
-printf "$timestamp,$num_cpus,$cpu_utilization,$loadavg_1m,$loadavg_5m,$loadavg_15m"
--- memStats.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-memAvail=`free -k | head -2 | tail -1 | awk '{print $7}'`
-memTot=`free -k | head -2 | tail -1 | awk '{print $2}'`
-timestamp=`date +%s%N | cut -b1-13`
-printf "$timestamp,$memAvail,$memTot"
